17d4b63ac0d77476da8e71bb1df4492b726e2635
r-jester <kingofdyroth007@gmail.com>
2025-06-26 18:25:12 +0700

    test auto re-deploy in vercel

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 function HomePage() {
-    return <h1 className="text-xl">Welcome to Home Page</h1>;
+    return <h1 className="text-xl">Welcome to My Home Page</h1>;
   }
   export default HomePage;
   
